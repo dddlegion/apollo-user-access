66d2847b247ec20e0f82c58ed3999f7d5e6f7fce
Andrii Vozniuk <a.vozniuk00@gmail.com>
2022-04-12 01:48:40 +0200

    trying to fix app

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -9,6 +9,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+string? port = Environment.GetEnvironmentVariable("PORT");
+
+if (!string.IsNullOrWhiteSpace(port))
+{
+    app.Urls.Add("http://*:" + port);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
